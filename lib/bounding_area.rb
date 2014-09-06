ce0d2bfd7a7cea8ba983f5127e41258590219c07
louis hoang <bawsing@gmail.com>
2014-09-06 16:22:06 -0400

    done

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,4 +1,13 @@
 class BoundingArea
-  def initialize
+  attr_reader :rectangles
+  def initialize(rectangles)
+    @rectangles = rectangles
+  end
+
+  def contains_point?(x, y)
+    rectangles.each do |rect|
+      return true if rect.contains_point?(x, y)
+    end
+    false
   end
 end
